153d9ec054db9bb4bcff77db98ace0ff29cf6385
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-24 16:54:22 +0530

    added methods

--- IntelliSurgery/Controllers/SurgeriesApiController.cs
+++ IntelliSurgery/Controllers/SurgeriesApiController.cs
@@ -1,5 +1,9 @@
 ﻿using IntelliSurgery.DbOperations;
+using IntelliSurgery.Global;
+using IntelliSurgery.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace IntelliSurgery.Controllers
 {
@@ -7,11 +11,31 @@ namespace IntelliSurgery.Controllers
     [Route("api/[controller]/[action]")]
     public class SurgeriesApiController : Controller
     {
-        private readonly ISurgeryTypeRepository surgeryRepository;
+        private readonly ISurgeryRepository surgeryRepository;
+        private readonly IAppointmentRepository appointmentRepository;
 
-        public SurgeriesApiController(ISurgeryTypeRepository surgeryRepository)
+        public SurgeriesApiController(ISurgeryRepository surgeryRepository, IAppointmentRepository appointmentRepository)
         {
             this.surgeryRepository = surgeryRepository;
+            this.appointmentRepository = appointmentRepository;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTableData()
+        {
+            List<Appointment> appointments = await appointmentRepository.GetAppointments();
+            return Json(new { success = true, data = appointments });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSchedule()
+        {
+            List<Appointment> appointments = await appointmentRepository.GetAppointments();
+            
+            SurgeryScheduler scheduler = new SurgeryScheduler(surgeryRepository);
+            await scheduler.CreateSchedule(appointments);
+            return Json(new { success = true });
+        }
+
     }
 }

d42bb76ff13396b302e669b1d709684db140e539
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-24 23:01:27 +0530

    example

---
+++ IntelliSurgery/wwwroot/js/calendar.js
@@ -0,0 +1,17 @@
+﻿async function GetScheduledSurgeries() {
+    var res = await axios.get("/api/CalendarApi/GetScheduledSurgeries");
+    var dto = {};
+    var surgeries = [];
+    if (res.data.success == true) {
+        dto = res.data.data;
+        surgeries = dto.scheduledSurgeries;
+    }
+    return surgeries;
+}
+
+$(document).ready(async () => {
+    var scheduledSurgeries = await GetScheduledSurgeries();
+    scheduledSurgeries.forEach(s => {
+        console.log(s);
+    });
+});

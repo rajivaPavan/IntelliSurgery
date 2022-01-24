89169d1145d5d278eec89d8a006c8a88453b4c2e
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-24 12:05:15 +0530

    updated operations

--- IntelliSurgery/DbOperations/Surgeons/ISurgeonRepository.cs
+++ IntelliSurgery/DbOperations/Surgeons/ISurgeonRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace IntelliSurgery.DbOperations
+﻿using IntelliSurgery.Models;
+using System.Threading.Tasks;
+
+namespace IntelliSurgery.DbOperations
 {
     public interface ISurgeonRepository
-    { 
+    {
+        Task<Surgeon> GetSurgeonById(int id);
     }
 }
--- IntelliSurgery/DbOperations/Surgeons/SurgeonRepository.cs
+++ IntelliSurgery/DbOperations/Surgeons/SurgeonRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace IntelliSurgery.DbOperations
+﻿using IntelliSurgery.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace IntelliSurgery.DbOperations
 {
     public class SurgeonRepository : ISurgeonRepository
     {
+        private readonly AppDbContext context;
+
+        public SurgeonRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<Surgeon> GetSurgeonById(int id)
+        {
+            return await context.Surgeons.FirstOrDefaultAsync(s => s.Id == id);
+        }
     }
 }

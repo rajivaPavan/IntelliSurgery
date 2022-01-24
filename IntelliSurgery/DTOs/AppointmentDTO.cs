9d35ba653444e0e1d49d87fd8e963f6f5756478e
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-24 11:55:38 +0530

    changed some data types

--- IntelliSurgery/DTOs/AppointmentDTO.cs
+++ IntelliSurgery/DTOs/AppointmentDTO.cs
@@ -2,9 +2,12 @@
 {
     public class AppointmentDTO
     {
+        public int PatientId { get; set; }
         public int SurgeryType { get; set; }
         public int DoctorId { get; set; }
         public bool IsAnesthesiaRequired { get; set; }
-        public int AnesthesiaType { get; set; }
+        public string AnesthesiaType { get; set; }
+        public string PriorityLevel { get; set; }
     }
+
 }

893618e7d946671cf6ec463afede35eab72b14d0
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-24 16:53:46 +0530

    added method

---
+++ IntelliSurgery/Global/PythonScriptRunner.cs
@@ -0,0 +1,29 @@
+﻿using IronPython.Hosting;
+using System;
+using System.IO;
+
+namespace IntelliSurgery.Global
+{
+    public class PythonScript
+    {
+        public TimeSpan PredictTime()
+        {
+            var engine = Python.CreateEngine();
+            //reading code from file
+            var source = engine.CreateScriptSourceFromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PythonSampleIronPython.py"));
+            var scope = engine.CreateScope();
+            //executing script in scope
+            source.Execute(scope);
+            var classCalculator = scope.GetVariable("calculator");
+            //initializing class
+            var calculatorInstance = engine.Operations.CreateInstance(classCalculator);
+
+            //https://www.dotnetlovers.com/Article/216/executing-python-script-from-C-Sharp
+
+            TimeSpan res = new TimeSpan(0,0,0);
+            return res;
+        }
+    }
+
+    
+}
---
+++ IntelliSurgery/Global/SurgeryScheduler.cs
@@ -0,0 +1,30 @@
+﻿
+using IntelliSurgery.DbOperations;
+using IntelliSurgery.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IntelliSurgery.Global
+{
+    public class SurgeryScheduler
+    {
+        private readonly ISurgeryRepository surgeryRepository;
+
+        public SurgeryScheduler(ISurgeryRepository surgeryRepository)
+        {
+            this.surgeryRepository = surgeryRepository;
+        }
+
+        public Task CreateSchedule(List<Appointment> appointments)
+        {
+            //
+            //implement algorithm
+            //make necessary updates in the database
+            //
+            //
+
+            return Task.CompletedTask;
+        }
+        
+    }
+}

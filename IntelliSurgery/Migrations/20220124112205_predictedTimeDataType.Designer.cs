d651344285446de86692c4f6de74a094c0b4dd3b
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-24 16:53:52 +0530

    update

--- IntelliSurgery/Migrations/20220124112205_predictedTimeDataType.Designer.cs
+++ IntelliSurgery/Migrations/20220124112205_predictedTimeDataType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IntelliSurgery.DbOperations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IntelliSurgery.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220124112205_predictedTimeDataType")]
+    partial class predictedTimeDataType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IntelliSurgery/Migrations/20220124112205_predictedTimeDataType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntelliSurgery.Migrations
+{
+    public partial class predictedTimeDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "PredictedTimeDuration",
+                table: "Appointments",
+                type: "time(6)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "PredictedTimeDuration",
+                table: "Appointments",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time(6)");
+        }
+    }
+}

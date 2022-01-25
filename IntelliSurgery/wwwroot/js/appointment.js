e6cba7a703dce2a60b498c79108574a76f4f9737
Sulith321 <sulith1perera@gmail.com>
2022-01-25 20:40:18 +0530

    edited appointment.js file

--- IntelliSurgery/wwwroot/js/appointment.js
+++ IntelliSurgery/wwwroot/js/appointment.js
@@ -1,3 +1,49 @@
-﻿var data = {
-    Name : 
+﻿const { axios } = require("../lib/axios/axios");
+
+
+// This function is used to get the list of surgeries from the backend
+async function getSurgeryTypes() {
+    var res = await axios.get("/api/AppoinmentApi/getSurgeryTypes");
+    var dto = {};
+    var surgeryTypes = [];
+    if (res.data.success == true) {
+        dto = res.data.data;
+        surgeryTypes = dto.surgeryTypes;
+    }
+    return surgeryTypes;
 }
+
+
+$(document).ready(async () => {
+    var surgeryTypes = await getSurgeryTypes();
+    surgeryTypes.forEach(s => {
+        console.log(s);
+    });
+});
+
+
+//This function is used to get the list of surgeons from the backend
+async function getSurgeons() {
+    var res = await axios.get("/api/AppoinmentApi/getSurgeons");
+    var dto = {};
+    var surgeons = [];
+    if (res.data.success == true) {
+        dto = res.data.data;
+        surgeons = dto.surgeons;
+    }
+    return surgeons;
+}
+
+
+$(document).ready(async () => {
+    var surgeons = await getSurgeons();
+    surgeons.forEach(s => {
+        console.log(s);
+    });
+});
+
+
+
+//var data = {
+//    Name : 
+//}

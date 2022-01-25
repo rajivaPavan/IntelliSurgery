2ca16a71e7ecfc9dfa000a965e37dfa3bf506292
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-25 20:52:31 +0530

    reviewed

--- IntelliSurgery/wwwroot/js/appointment.js
+++ IntelliSurgery/wwwroot/js/appointment.js
@@ -1,45 +1,57 @@
-﻿const { axios } = require("../lib/axios/axios");
+﻿//// This function is used to get the list of surgeries from the backend
+//async function getSurgeryTypes() {
+//    var res = await axios.get("/api/AppoinmentApi/getSurgeryTypes");
+//    var dto = {};
+//    var surgeryTypes = [];
+//    if (res.data.success == true) {
+//        dto = res.data.data;
+//        surgeryTypes = dto.surgeryTypes;
+//    }
+//    return surgeryTypes;
+//}
 
+////This function is used to get the list of surgeons from the backend
+//async function getSurgeons() {
+//    var res = await axios.get("/api/AppoinmentApi/getSurgeons");
+//    var dto = {};
+//    var surgeons = [];
+//    if (res.data.success == true) {
+//        dto = res.data.data;
+//        surgeons = dto.surgeons;
+//    }
+//    return surgeons;
+//}
 
-// This function is used to get the list of surgeries from the backend
-async function getSurgeryTypes() {
-    var res = await axios.get("/api/AppoinmentApi/getSurgeryTypes");
+//
+/////////////////////////////////////// Sulith Google whats document ready scene eka ////////////////////////////////////
+//
+
+//$(document).ready(async () => {
+//    var surgeryTypes = await getSurgeryTypes();
+//    surgeryTypes.forEach(s => {
+//        console.log(s);
+//    });
+//});
+
+async function getDropDownLists() {
+    var res = await axios.get("/api/AppoinmentApi/GetFormDropDownLists");
     var dto = {};
-    var surgeryTypes = [];
+    var dropDownLists = {
+        surgeons: [],
+        surgeryTypes:[]
+    }
     if (res.data.success == true) {
         dto = res.data.data;
-        surgeryTypes = dto.surgeryTypes;
+        dropDownList.surgeons = dto.surgeons;
+        dropDownList.surgeryTypes = dto.surgeryTypes;
     }
-    return surgeryTypes;
+    return dropDownLists;
 }
 
 
-$(document).ready(async () => {
-    var surgeryTypes = await getSurgeryTypes();
-    surgeryTypes.forEach(s => {
-        console.log(s);
-    });
-});
-
-
-//This function is used to get the list of surgeons from the backend
-async function getSurgeons() {
-    var res = await axios.get("/api/AppoinmentApi/getSurgeons");
-    var dto = {};
-    var surgeons = [];
-    if (res.data.success == true) {
-        dto = res.data.data;
-        surgeons = dto.surgeons;
-    }
-    return surgeons;
-}
-
 
 $(document).ready(async () => {
-    var surgeons = await getSurgeons();
-    surgeons.forEach(s => {
-        console.log(s);
-    });
+    //this function is called automatically when the html is fully loaded
 });
 
 

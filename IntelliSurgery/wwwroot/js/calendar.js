fbf1b599d47ae36f389034b28e850a61a04afca5
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-27 19:37:33 +0530

    added full calendar

---
+++ IntelliSurgery/wwwroot/js/calendar.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        themeSystem: "bootstrap",
+        views: {
+            dayGridMonth: { // name of view
+                titleFormat: { year: 'numeric', month: '2-digit', day: '2-digit' }
+                // other view-specific options here
+            }
+        },
+        headerToolbar: {
+            start: 'title', // will normally be on the left. if RTL, will be on the right
+            center: '',
+            end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
+        },
+        height:1000
+    });
+
+    calendar.render();
+});

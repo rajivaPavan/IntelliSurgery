91787d35a65e85e512278d2b64c09907b168d51b
SuveenE <suveen.te1@gmail.com>
2022-01-29 22:15:12 +0530

    update

---
+++ IntelliSurgery/wwwroot/js/admin/surgery.js
@@ -0,0 +1,38 @@
+﻿function getNewSurgery() {
+    var newSurgery = $('#surgery').val();
+
+    var surgery = {
+        name: newSurgery
+    }
+
+    return surgery;
+}
+
+async function addSurgeryRequest(Surgery) {
+
+    const NULL_SURGERY_ID = 0;
+    var surgeryId = NULL_SURGERY_ID;
+
+    var res = await axios.post("/api/Admin/AddSurgery", surgery);
+    if (res.data.success == true) {
+        surgeryId = res.data.data;
+    }
+    return surgeryId;
+}
+
+async function addSurgery() {
+
+    var surgery= getNewSurgery();
+    var surgeryId = await addSurgeryRequest(surgery);
+    global.addedSurgeryId = surgeryId;
+}
+
+function clearNewSurgeryField() {
+    $('#surgery').val("");
+}
+
+function showSurgeryDetails() {
+    var message = $('#surgery').val();
+    display_message.innerHTML = message;
+}
+

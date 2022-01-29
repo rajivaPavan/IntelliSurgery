d3ec4a5fc5d77cf6c463e5408d19955b63840347
SuveenE <suveen.te1@gmail.com>
2022-01-30 00:55:23 +0530

    hi

--- IntelliSurgery/wwwroot/js/admin/speciality.js
+++ IntelliSurgery/wwwroot/js/admin/speciality.js
@@ -1,39 +1,22 @@
-﻿function getNewSpeciality(){
+﻿var specialityInput = [];
+
+function getNewSpeciality() {
     var newSpeciality = $('#speciality').val();
 
     var speciality = {
         name: newSpeciality
     }
 
-    return speciality;
-}
-
-async function addSpecialityRequest(speciality) {
-
-    const NULL_SPECIALITY_ID = 0;
-    var specialityId = NULL_SPECIALITY_ID;
-
-    var res = await axios.post("/api/AdminApi/SaveHospitalData", hospitalData.Specialities);
-    if (res.data.success == true) {
-        specialityId = res.data.data;
-    }
-    return specialityId;
-}
-
-async function addSpeciality() {
-
-    var speciality = getNewSpeciality();
-    var specialityId = await addSpecialityRequest(speciality);
-    global.addedSpecialityId = speciality;
+    specialityInput.push(speciality);
 }
 
 function clearNewSpecialityField(){
-    $('#surgery_type').val("");
+    $('#speciality').val("");
 }
 
-function showSpecialityDetails() {
-    var message = $('#surgery_type').val();
-    display_message.innerHTML = message;
+function showNewSpecialityDetails() {
+    var message = $('#speciality').val();
+    display_speciality.innerHTML = message;
 }
 
-//display_message should be the id in the html part where the msg is displayed
+//display_speciality should be the id in the html part where the msg is displayed

5787b2c7a1939334780640189e963be894252afc
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-29 23:05:47 +0530

    example, check this

--- IntelliSurgery/wwwroot/js/admin/requests.js
+++ IntelliSurgery/wwwroot/js/admin/requests.js
@@ -11,3 +11,21 @@
     }
     return dropDownLists;
 }
+
+async function saveHospitalDataRequest(hospitalData) {
+    // hospitalData parameter show look like the follow object
+    // {
+    //    Surgeons: [ { Name: "Dr.ABC", SpecialityId: 1 }, similar_objects...],
+    //    Specialities: [ { Name: "Neurologist" }, { Name: "Cardiac Surgeon"}],
+    //    SurgeryTypes: [{ Name: "type asd" },......],
+    //    TheatreTypes: [{ Name: "type asd" },......],
+    //    Theatres: [{ Name: "OR 1", TheatreTypeId: 1 }, {}],
+    //    SurgeryTypeTheatres: [{ SurgeryTypeId: 1, TheatreIds: [1, 2, 3, 4] }, ....]
+    // }
+
+    //sending the data to the backend
+    var res = axios.post("/api/AdminApi/SaveHospitalData", hospitalData);
+
+    //nothing meaningful to do with variable res here so just end the function
+    return;
+}

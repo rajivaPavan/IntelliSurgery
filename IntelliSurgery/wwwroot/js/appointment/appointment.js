9afd081ee1d310393403ca64cb5d8427abeb1faa
Janeesha2001 <janeeshajwick@gmail.com>
2022-01-31 04:35:05 +0530

    validations

--- IntelliSurgery/wwwroot/js/appointment/appointment.js
+++ IntelliSurgery/wwwroot/js/appointment/appointment.js
@@ -23,3 +23,89 @@ function clearAllAppointmentFields() {
     $("#surgeon").val(-1);
     $("#anasthesia_type").val(-1);
 }
+
+async function validatePatient() {
+    
+
+
+    return patientId;
+}
+
+async function addPatient() {
+    
+
+
+    return patientId;
+}
+
+async function updatePatient() {
+
+
+
+
+    return patientId;
+}
+
+// Validate patient name
+$('#patientNameCheck').hide();
+let patientNameError = true;
+
+$('#patient_name').keyup(function () {
+    validatePatientName();
+});
+
+function validatePatientName() {
+    let patientNameValue = $('#patient_name').val();
+
+    if (patientNameValue.length == '') {
+        $('#patientNameCheck').show();
+        patientNameError = false;
+        return false;
+    }
+    else {
+        $('#patientNameCheck').hide();
+    }
+}
+
+//Validate Weight
+$('#weightCheck').hide();
+let weightError = true;
+
+$('#weight').keyup(function () {
+    validateWeight();
+});
+
+function validateWeight() {
+    let weight = $('#weight').val();
+
+    if (weight.length == '') {
+        $('#weightCheck').show();
+        weightError = false;
+        return false;
+    }
+    else {
+        $('#weightCheck').hide();    //need to check whether isNumeric(),
+    }
+}
+
+
+//Validate Height
+$('#heightCheck').hide();
+let heightError = true;
+
+$('#height').keyup(function () {
+    validateHeight();
+});
+
+function validateHeight() {
+    let height = $('#height').val();
+
+    if (height.length == '') {
+        $('#heightCheck').show();
+        heightError = false;
+        return false;
+    }
+    else {
+        $('#heightCheck').hide();             //need to check whether isNumeric(),
+    }
+}

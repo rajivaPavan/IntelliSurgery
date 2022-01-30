c4891d0808c04d46a0dc2554ae78a66fcfcc76ab
rajivaPavan <rajivapitiwaduge@gmail.com>
2022-01-30 13:50:15 +0530

    update

--- IntelliSurgery/wwwroot/js/calendar/calendar-main.js
+++ IntelliSurgery/wwwroot/js/calendar/calendar-main.js
@@ -4,19 +4,31 @@ calendarApp = Vue.createApp({
     data() {
         return {
             calendars: {},
-            filters: {
-                
-            },
+            filters: initFilters(),
             fitlerValues: {},
             selectedFilter: "",
             selectedFilterValue:-1
         };
     },
     computed: {
+        getSelectedFilterText() {
+            var text = "a filter first";
+            if (this.selectedFilter == "") {
+                $("#filterValuesDropDown").prop("disabled", true);
+                return text;
+            }
+            $("#filterValuesDropDown").prop("disabled", false);
+            var selectedF = this.filters.find((filter) => {
+                return filter.value == this.selectedFilter;
+            });
+
+            text = selectedF.text;
+            return text;
+        },
         getFilterValueOptions() {
             var selectedFilter = this.selectedFilter;
             if (selectedFilter != "") {
-                return filterValues[selectedFilter];
+                return this.filterValues[selectedFilter];
             }
             return [];
         },
@@ -33,7 +45,22 @@ calendarApp = Vue.createApp({
 calendarVueApp = calendarApp.mount("#calendar-main");
 
 $(document).ready(async () => {
-    initCalendar([]);
-    var filterValues = await getDropDownListsRequest();
+    //init empty calendar
+    var noEvents = [];
+    initCalendar(noEvents);
+
+    //init dropdowns
+    var filterValues = await getFilterValuesRequest();
     calendarVueApp.filterValues = filterValues;
 });
+
+
+function initFilters() {
+    var filters = [
+        { text:"Surgeon", value:"surgeons" },
+        { text:"Theatre", value:"theatres" },
+        { text: "Theatre Type", value: "theatreTypes" },
+        { text: "Surgery Type", value: "surgeryTypes" }
+    ];
+    return filters;
+}

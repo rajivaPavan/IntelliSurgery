614cb6df046a6c6bc51efb13d0ab408191923dc1
Sulith321 <sulith1perera@gmail.com>
2022-02-03 16:12:14 +0530

    Not a joke

--- IntelliSurgery/Global/MLModelRunner.py
+++ IntelliSurgery/Global/MLModelRunner.py
@@ -3,28 +3,34 @@ import sys
 import json
 import re
 
-file_path = "C:\\Users\\pavan\\Documents\\After Alevel\\Projects\\IntelliSurgery\\IntelliSurgery\\MLModels\\model.pkl"
+file_path = "C:\\Users\\Sulith\\Desktop\\intelliSurgery\\IntelliSurgery\\IntelliSurgery\\MLModels\\model.pkl"
 loaded_model = []
 with open(file_path, 'rb') as f:
     loaded_model = pickle.load(f)
 
 
 #Using the data passed to the file
-inputData = str(sys.argv[1])
+age  = (sys.argv[0])
+gender = (sys.argv[1])
+asa = (sys.argv[2])
+bmi = (sys.argv[3])
+complication = (sys.argv[4])
+SurgeryType = (sys.argv[5])
+#diseases = (sys.argv[7])
 
 #adds the missing double quoutes to the keys
-inputData = re.sub("(\w+):", r'"\1":',  inputData)
+#inputData = re.sub("(\w+):", r'"\1":',  inputData)
 
-dataDictionary = json.loads(inputData)
+#dataDictionary = json.loads(inputData )
 
 #dataDictionary = {"Age":37,"Gender":1,"ASA":0,"BMI":0.0,"Diseases":["Cancer","Cardiovascular"],"Complication":0,"SurgeryType":"Cardiovascular Surgery"}
-
+''''
 age = dataDictionary["Age"]
 gender = dataDictionary["Gender"]
 asa = dataDictionary["ASA"]
 bmi = dataDictionary["BMI"]
 diseasesList = dataDictionary["Diseases"]
-
+'''
 
 #Since the diseases related to the patient are passed, initial values are set to zero.
 #If the patient has a disease then the value will be changed to 1.
@@ -37,7 +43,7 @@ osteoarthritis = 0
 pyschologicalDisorder = 0
 pulmonary = 0
 
-
+'''
 if (len(diseasesList)== 0):
     pass
 else:
@@ -60,13 +66,37 @@ else:
             pulmonary = 1
         else:
             pass
-        
-machineLearningModelInput = [[age,asa,gender,bmi,cancer,cardiovascular,dementia,diabetes,digestive,osteoarthritis,pyschologicalDisorder,pulmonary]]
-
+      
+if  "Cancer" in diseases:
+    cancer = 1
+elif  "Cardiovascular" in diseases:
+    cardiovascular = 1
+elif  "Dementia" in diseases:
+    dementia =1
+elif  "Diabetes" in diseases:
+    diabetes = 1
+elif "Digestive" in diseases:
+    digestive = 1
+elif  "Osteoarthritis" in diseases:
+    osteoarthritis = 1
+elif  "PyschologicalDisorder" in diseases:
+    pyschologicalDisorder = 1
+elif  "Pulmonary" in diseases:
+    pulmonary = 1
+else:
+    pass
+''' 
+def hello(a):
+    print("Hello machine learinig",a)
+    return 9.92085089;
+#machineLearningModelInput = [[age,asa,gender,bmi,cancer,cardiovascular,dementia,diabetes,digestive,osteoarthritis,pyschologicalDisorder,pulmonary]]
+''''
 def predictTheTime(modelInput):
     "This function is used to predict the time for the given surgery"
     predictedOutput = loaded_model.predict(modelInput)
     #print(modelInput, predictedOutput)
     return predictedOutput[0][0];
 
-print(predictTheTime(machineLearningModelInput))
+print(predictTheTime(machineLearningModelInput))'''
+
+print(hello(cancer))
